fd110e182bcc0215f5de3aec899b12ddda84a873
zascogm <zascogm@gmail.com>
2020-08-19 08:37:19 +1000

    adding new routes, adding switch component to app.js, adding functionality to navbar and about pages

--- src/App.js
+++ src/App.js
@@ -3,18 +3,28 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 import Home from './pages/Home'
 import About from './pages/About'
-// import SingleCocktail from './pages/SingleCocktail'
-// import Error from './pages/Error'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
     </Router>
-  )
+  );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,5 +2,16 @@ import React from "react";
 
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed 
+      consequat ligula risus, vel congue lectus maximus pellentesque. 
+      Ut accumsan, odio et scelerisque vehicula, risus eros pharetra 
+      magna, ut hendrerit dui purus at risus. Sed lacinia erat id 
+      justo auctor tristique.
+      </p>
+    </section>
+  );
 }
